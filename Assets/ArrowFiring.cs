e56f5be8f6cb547ce01a789c3bac68787e23742a
DylanL0ng <dylanhussain08@gmail.com>
2020-03-05 21:18:02 +0000

    Add base

---
+++ Assets/ArrowFiring.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArrowFiring : MonoBehaviour
+{
+    [SerializeField] private GameObject arrowPrefab;
+    [SerializeField] private Transform arrowSpawn;
+    [SerializeField] private float arrowForce = 20f;
+    void Update()
+    {
+        if(Input.GetButtonDown("Fire1"))
+        {
+            GameObject arrow = Instantiate(arrowPrefab ,arrowSpawn.position, Quaternion.identity);
+            arrow.transform.Rotate(new Vector3(90, 0 ,0));
+            arrow.GetComponent<Rigidbody>().AddForce(gameObject.transform.forward * arrowForce, ForceMode.VelocityChange);
+        }
+        
+    }
+}
---
+++ Assets/arrowScript.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class arrowScript : MonoBehaviour
+{
+    [SerializeField] private Rigidbody myBody;
+    private bool isCollided = false;
+
+    void Start()
+    {
+        myBody = gameObject.GetComponent<Rigidbody>();        
+    }
+
+    void Update()
+    {
+        if (!isCollided)
+        {
+            gameObject.transform.Rotate(Vector3.forward * Random.Range(0, 5) * Time.deltaTime);
+            gameObject.transform.Rotate(Vector3.up * Random.Range(180, 960) * Time.deltaTime);
+        }
+        
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        isCollided = true;
+        Stick();
+        Destroy(gameObject, 10f);
+        
+            Debug.Log($"Hit {collision.gameObject.name}");
+    }
+
+    private void Stick()
+    {
+        myBody.constraints = RigidbodyConstraints.FreezeAll;
+    }
+}
